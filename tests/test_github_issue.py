e7a2f6669974c3ca864ae982041b1877dcd6d35a
Viacheslav Suchkov <iexploudi@gmail.com>
2025-06-23 07:35:01 +0400

    Работа с allure в разныех форматах

--- tests/test_github_issue.py
+++ tests/test_github_issue.py
@@ -1,3 +1,4 @@
+import allure
 from selene import browser, be, by
 
 
@@ -9,3 +10,51 @@ def test_github_issue():
     browser.element("[href='/IExpLouDI/qaguru_autotest_lesson_5/issues']").click()
 
     browser.element("//a[text()='Issue for allure test']").should(be.visible)
+
+
+def test_github_with_lambda_steps():
+    with allure.step("Открываем гит хаб"):
+        browser.open("https://github.com/")
+
+    with allure.step("Открываем строку поиска и вносим название репозитория"):
+        browser.element(".header-search-button").click()
+        browser.element("#query-builder-test").type("qaguru_autotest_lesson_5").press_enter()
+
+    with allure.step("Выбираем найденный репозиторий"):
+        browser.element("[href='/IExpLouDI/qaguru_autotest_lesson_5']").click()
+
+    with allure.step("Переходим в раздел issues"):
+        browser.element("[href='/IExpLouDI/qaguru_autotest_lesson_5/issues']").click()
+
+    with allure.step("Проверяем наличие issues с требуемым названием"):
+        browser.element("//a[text()='Issue for allure test']").should(be.visible)
+
+
+def test_github_with_decorator_steps():
+    open_git("https://github.com/")
+    select_button_and_type_repo("qaguru_autotest_lesson_5")
+    select_repo("qaguru_autotest_lesson_5")
+    open_repo_issue()
+    should_issue_be_visible("Issue for allure test")
+
+
+@allure.step("Открываем гит хаб")
+def open_git(url:str):
+    browser.open(url)
+
+@allure.step("Открываем строку поиска и вносим название репозитория")
+def select_button_and_type_repo(repo_name:str):
+    browser.element(".header-search-button").click()
+    browser.element("#query-builder-test").type(repo_name).press_enter()
+
+@allure.step("Выбираем найденный репозиторий")
+def select_repo(repo_name:str):
+    browser.element(f"[href='/IExpLouDI/{repo_name}']").click()
+
+@allure.step("Переходим в раздел issues")
+def open_repo_issue():
+    browser.element("[href='/IExpLouDI/qaguru_autotest_lesson_5/issues']").click()
+
+@allure.step("Проверяем наличие issues с требуемым названием")
+def should_issue_be_visible(some_text:str):
+    browser.element(f"//a[text()='{some_text}']").should(be.visible)
